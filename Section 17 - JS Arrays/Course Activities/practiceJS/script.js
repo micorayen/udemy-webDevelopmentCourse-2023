1cf7ae156c38105ed09a5d64aea87b4a020635f5
micorayen <micorayenaniga@gmail.com>
2023-07-17 20:15:25 +0800

    update files
    commit Course's section 17 - JS Arrays

---
+++ Section 17 - JS Arrays/Course Activities/practiceJS/script.js
@@ -0,0 +1,58 @@
+console.log("Section 17: JavaScript Arrays:")
+
+// Topic: Introducing Array  --------------------
+// Topic: Array Random Access  --------------------
+// let dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+// // dayOfWeek[6] = 'Sunday'
+// dayOfWeek[5] = 'Saturday'
+// console.log(dayOfWeek)
+
+// Topic: Array: Push an0-d Pop (Happened in the End) --------------------
+// let pvpList = ['Jones', 'Volkanovski', 'Makachev']
+// console.log(pvpList)
+
+// pvpList.push('Usman')
+// console.log(pvpList)
+
+// pvpList.pop()
+// console.log(pvpList)
+
+// Topic: Array: Shift and Unshift (Happened in the start Start)  --------------------
+// let movieLine = ['Mico', 'Marco', 'John']
+// console.log(movieLine)
+
+// movieLine.shift()
+// console.log(movieLine)
+
+// movieLine.unshift('VIP')
+// console.log(movieLine)
+
+// Topic: Other Array Methods: Concat, IndexOf, Includes & Reverse  --------------------
+// Concat: ----------
+// const array1 = ['a', 'b', 'c']
+// const array2 = ['d', 'e', 'f']
+// const array3 = array1.concat(array2)
+
+// console.log(array3)
+
+// Includes: Boolean Method throws true or false ----------
+// console.log(array3.includes('cs'))
+
+// IndexOf - -1 if not found----------
+// const pvpList = ['Jones', 'Volk', 'Makachev']
+// console.log(pvpList.indexOf('Makachev'))
+
+// Reverse: Destructive - reverse values declared----------
+// pvpList.reverse()
+// console.log(pvpList)
+
+// Topic: Other Array Methods: Slice, Splice  --------------------
+// const dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+// console.log(dayOfWeek.slice(2))
+
+// const ranks = ["1st", "2nd", "3rd", "4th", "5th"]
+// ranks.splice(2, 2, '6th', '7th')
+// console.log(ranks)
+
+
+
