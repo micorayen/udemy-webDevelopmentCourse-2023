fea08cb02e4f4c6d7a266da40086a7f1dfcd04bc
micorayen <micorayenaniga@gmail.com>
2023-07-18 19:08:44 +0800

    update files
    commit Course's section 18 - JavaScript Object Literals

---
+++ Section 18 - JavaScript Object Literals/Course Activities/practiceobjectLiterals.js
@@ -0,0 +1,74 @@
+// Sample of Properties of an array [not cery helpful cause lacks of labels]
+// let arrayFitBit = [30800, 1200]
+// console.log(arrayFitBit)
+
+// Object Literals
+// ie1:
+// const person = {
+//     firstName: 'Mico',
+//     lastName: 'Rayen',
+//     age: 24,
+//     nationality: 'Filipino'
+// }
+// console.log(person)
+
+// ------------------------------------------------
+
+// Topic: Accessing Data out of Objects --------------------
+// let comment = {
+//     username: 'eagle29-0',
+//     downVotes: 19,
+//     upVotes: 214,
+//     netScore: 195,
+//     commentText: 'Tap like chicken',
+//     tags: ['#hillarious', '#Funny', 'silly'],
+//     isGilded: false
+// }
+
+// console.log(comment["isGilded"])
+// console.log(comment.commentText)
+
+// Topic: Modifying Objects --------------------
+// const prelims = {
+//     mico: 96,
+//     marco: 78
+// }
+
+// console.log(prelims)
+
+// // Now lets modify values
+// prelims.mico = 'A'
+// console.log(prelims.mico)
+// prelims.marco = 'C+'
+// console.log(prelims.marco)
+
+// console.log(prelims)
+
+// // Add additionals 'Key and Values' or Property
+// prelims.rezy = 'B+'
+// prelims['john'] = 'A-'
+
+// console.log(prelims)
+
+// Topic: Nesting Arrays & Objects --------------------
+const comments = [
+    { username: 'Mico', Text: 'That was funny!', votes: 3 },
+    { username: 'NF', Text: 'My new album, is Out!', votes: 12212 }
+]
+
+console.log(comments[1])
+console.log(comments[1].Text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
